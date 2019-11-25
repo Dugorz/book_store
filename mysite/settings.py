e34dc7a36ce9ea557596686b9fbc9c76616ac4d3
Dugorz <38889200+Dugorz@users.noreply.github.com>
2019-11-25 18:10:21 +0200

    Book store

--- mysite/settings.py
+++ mysite/settings.py
@@ -20,7 +20,7 @@ BASE_DIR = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
 # See https://docs.djangoproject.com/en/2.2/howto/deployment/checklist/
 
 # SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY = '616ka8ag6$o^^hlbs&(*6rtq=x6hgk&hah!u+8q)@93gf5rdxl'
+SECRET_KEY = os.environ.get('SECRET_KEY')
 
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
